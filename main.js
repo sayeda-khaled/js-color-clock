0e64d06007c38af08f598ab40ee6883afca0074d
Sayeda Khaled <sayedakhaled@Sayedas-MacBook-Pro.local>
2021-05-05 21:43:32 -0400

    created the clock and the background change

--- main.js
+++ main.js
@@ -1,4 +1,41 @@
-const currentDate = new Date();
-console.log(currentDate);
 
-setInterval(function(){ console.log(currentDate); }, 1000);
+//window.onload = function(){
+
+
+
+
+//console.log(currentDate);
+
+let clockDisplay = document.querySelector('.clock-display');
+
+let clockColor = document.querySelector('.clock');
+
+setInterval(function(){
+  let currentDate = new Date();
+
+  //console.log(currentDate.toLocaleTimeString());
+
+clockDisplay.innerHTML = currentDate.toLocaleTimeString(); //.innerHTML to change HTML
+
+  //clockDisplay.innerHTML = currentDate; //.innerHTML to change HTML.. Used the above, so that I can change the time format
+
+  let randomColor = Math.floor(Math.random()*16777215).toString(16); //generating random colors
+
+  clockColor.style.backgroundColor = "#" + randomColor; //.style for css formating
+  //console.log(randomColor);
+}, 1000);
+
+
+
+//const clock-display = document.querySelector('.clock-display');
+
+
+
+
+//clockDisplay = "Hey!";
+//console.log(clockDisplay);
+
+
+
+
+//}
